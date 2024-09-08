403c524fdaad99bcacc49134d7b4f654715605e1
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-09-08 09:55:10 +0200

    Configure .env

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -2,3 +2,5 @@ import { DataAPIClient } from "@datastax/astra-db-ts";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import "dotenv/config";
 import OpenAI from "openai";
+
+
