e83cf18e08f31f3d489f74765fc857e3fff44ebd
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-09-08 10:13:04 +0200

    Save work

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -3,4 +3,29 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import "dotenv/config";
 import OpenAI from "openai";
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
+const client = new DataAPIClient(process.env.ASTRA_DB_APPLICATION_TOKEN);
+const db = client.db(process.env.ASTRA_DB_API_ENDPOINT, {
+  namespace: process.env.ASTRA_DB_NAMESPACE,
+});
+
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  chunkOverlap: 200,
+});
+
+const createCollection = async () => {
+  try {
+    const res = await db.createCollection("characters", {
+      vector: {
+        dimension: 1536,
+      },
+    });
+    console.log(res);
+  } catch (e) {
+    console.log("");
+  }
+};
